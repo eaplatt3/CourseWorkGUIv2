30ba7b9bf97b1629cccb5781b543b31fd7d41e25
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-11-13 19:04:11 -0500

    added icon and menu

--- CourseWorkGUIv2/MainWindow.xaml.cs
+++ CourseWorkGUIv2/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace CourseWorkGUIv2
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
